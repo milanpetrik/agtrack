2c08b4a1495181af7176b052bb5a9c8b9f26c3a5
Milan <milan.petrik@protonmail.com>
2024-11-13 11:12:52 +0100

    Include agfile.py in agtrack.py

--- agtrack.py
+++ agtrack.py
@@ -3,25 +3,131 @@ import argparse
 import numpy as np
 from numpy.linalg import inv 
 from rotations import *
-from agfile import read_ag_file
 
 class AGTracker:
+    """
+    Attributes:
+        filename (str): name of the file with the measurements from
+            accelerometer and gyroscope
+        frequency (int): frequency in Hz
+        acc_range (int): accelerometer range as a multiple of g
+            (gravitational acceleration)
+        gyr_range (int): gyroscope range in degrees per second
+        grav (float): gravitational acceleration in m*s^(-2); `9.80665` by default
+        measurements (list of ((float, float, float), (float, float, float))):
+            list of `((ax, ay, az), (gx, gy, gz))` where `(ax, ay, az)` is
+            the acceleration and `(gx, gy, gz)` is the angular velocity
+            given by the gyroscope
+        quiet (bool): suppress warnings; `False` by default
+    """
 
     def __init__(self,
                  filename,
                  frequency = None,
                  acc_range = None,
                  gyr_range = None,
-                 grav = 9.80665):
-        self.ag_data = read_ag_file(filename,
-                                    frequency = frequency,
-                                    acc_range = acc_range,
-                                    gyr_range = gyr_range,
-                                    grav = grav)
+                 grav = 9.80665,
+                 quiet = False):
+        """
+        Args:
+            filename (str): name of the file with the measurements from
+                accelerometer and gyroscope
+            frequency (int): frequency in Hz
+            acc_range (int): accelerometer range as a multiple of g
+                (gravitational acceleration)
+            gyr_range (int): gyroscope range in degrees per second
+            grav (float): gravitational acceleration in m*s^(-2); `9.80665` by default
+            quiet (bool): suppress warnings; `False` by default
+        """
+        self.filename = filename
+        self.frequency = frequency
+        self.acc_range = acc_range
+        self.gyr_range = gyr_range
+        self.grav = grav
+        self.quiet = quiet
+        self.measurements = None
+        self.load()
+
+    def load(self):
+        """
+            Read a text file with the measurements from accelerometer and
+                gyroscope.
+
+            The data are parsed and saved to the `measurements` attribute:
+
+                * The acceleration data are multiplied by `self.acc_range/2`
+                    and by `self.grav`.
+                * The gyroscope data are multiplied by `self.gyr_range/2`.
+        """
+        coord_names = ['x', 'y', 'z'] # this is for reporting an error
+        if self.acc_range != None:
+            acc_coef = self.acc_range / 2
+        else:
+            acc_coef = None
+        if self.gyr_range != None:
+            gyr_coef = self.gyr_range / 2
+        else:
+            gyr_coef = None
+        self.measurements = []
+        with open(self.filename) as f:
+            line_counter = 0
+            # saturation counters count occurrences of `2.0` in the input data file
+            # for each coordinate of the acceleration and the angular speed given
+            # by the gyroscope
+            sat_counter = 6 * [0]
+            sat_begin = 6 * [None]
+            for line in f:
+                line_counter += 1
+                if line[0] == '#': # ignore lines starting with '#'
+                    continue
+                elif line[0].isalpha() and '=' in line:
+                    words = line.split('=')
+                    left_value = words[0].strip()
+                    right_value = words[1].strip()
+                    if left_value == 'frequency' and self.frequency == None:
+                        self.frequency = int(right_value)
+                    elif left_value == 'acc_range' and self.acc_range == None:
+                        self.acc_range = int(right_value)
+                        acc_coef = self.acc_range / 2
+                    elif left_value == 'gyr_range' and self.gyr_range == None:
+                        self.gyr_range = int(right_value)
+                        gyr_coef = self.gyr_range / 2
+                elif line[0].isdigit() or line[0] == '-':
+                    if self.acc_range == None:
+                        raise Exception('acc_range not defined')
+                    if self.gyr_range == None:
+                        raise Exception('gyr_range not defined')
+                    words = line.strip().replace(',', '.').split(';') # strip() removes "\n" at the end of the line
+                    numbers = list(map(float, words))
+                    # warnings
+                    if not self.quiet:
+                        for i in range(len(numbers)):
+                            if abs(numbers[i] - 2.0) < 0.0001:
+                                if sat_counter[i] == 0:
+                                    sat_counter[i] = 1
+                                    sat_begin[i] = line_counter
+                                else:
+                                    sat_counter[i] += 1
+                            else:
+                                if sat_counter[i] > 3:
+                                    if i < 3:
+                                        who = 'accelerometer'
+                                        crd_name = coord_names[i]
+                                    else:
+                                        who = 'gyroscope'
+                                        crd_name = coord_names[i - 3]
+                                    print('WARNING: {} saturated in {} on {} lines starting from line {}'.format(
+                                        who, crd_name, sat_counter[i], sat_begin[i]))
+                                sat_counter[i] = 0
+                                sat_begin[i] = None
+                    # save data
+                    acceleration = [v*acc_coef*self.grav for v in numbers[:3]]
+                    angular_velocity = [v*gyr_coef for v in numbers[3:]]
+                    self.measurements.append((tuple(acceleration), tuple(angular_velocity)))
 
     def parse(self):
         #print(self.ag_data['measurements'])
-        for (acc, gyr) in self.ag_data['measurements']:
+        for (acc, gyr) in self.measurements:
             print(acc, gyr)
 
     def load_file(self, filename, frequency, acc_range, gyr_range):
@@ -125,7 +231,7 @@ def main():
                           acc_range = acc_range,
                           gyr_range = gyr_range,
                           grav = grav)
-    #agtracker.parse()
+    agtracker.parse()
 
     if args.draw:
         agtracker.draw()
